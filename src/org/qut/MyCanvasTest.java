8c96144ea9f0237e5e26cf86a41e61e017d162bc
Unknown <jackson.grant1998@gmail.com>
2019-05-18 15:17:03 +1000

    Test Cases

--- src/org/qut/MyCanvasTest.java
+++ src/org/qut/MyCanvasTest.java
@@ -1,5 +1,6 @@
 package org.qut;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyCanvasTest {
@@ -9,72 +10,47 @@ class MyCanvasTest {
 
     @org.junit.jupiter.api.Test
     void resetCommands() {
-        System.out.println("test case reset commands");
-        CANVAS.addCommand("RECTANGLE");
-        CANVAS.resetCommands();
+        MyCanvas testCanvas = new MyCanvas();
+
+        System.out.println("test case reset command");
+        testCanvas.addCommand("RECTANGLE");
+        testCanvas.resetCommands();
         StringBuilder sb = new StringBuilder();
-        assertTrue(CANVAS.getDrawCommands().size() == 0);
+        assertTrue(testCanvas.getCommands().size() == 0);
     }
 
     @org.junit.jupiter.api.Test
     void addCommand() {
-        System.out.println("test case add commands");
-        CANVAS.addCommand("RECTANGLE");
+        MyCanvas testCanvas = new MyCanvas();
+
+        System.out.println("test case add command");
+        testCanvas.addCommand("RECTANGLE");
         StringBuilder sb = new StringBuilder();
-        for (String str : CANVAS.getDrawCommands()) {sb.append(str);}
+        for (String str : testCanvas.getCommands()) {sb.append(str);}
         assertEquals ("RECTANGLE", sb.toString());
     }
 
     @org.junit.jupiter.api.Test
     void undoLastCommand() {
+        MyCanvas testCanvas = new MyCanvas();
+
         System.out.println("test case undo last command");
-        CANVAS.addCommand("RECTANGLE");
-        CANVAS.addCommand("POLYGON");
-        CANVAS.undoLastCommand();
+        testCanvas.addCommand("RECTANGLE");
+        testCanvas.addCommand("POLYGON");
+        testCanvas.undoLastCommand();
         StringBuilder sb = new StringBuilder();
-        for (String str : CANVAS.getDrawCommands()) {sb.append(str);}
+        for (String str : testCanvas.getCommands()) {sb.append(str);}
         assertEquals ("RECTANGLE", sb.toString());
     }
 
     @org.junit.jupiter.api.Test
     void getCommands() {
-        System.out.println("test case get commands");
-        CANVAS.addCommand("RECTANGLE");
-        CANVAS.addCommand("POLYGON");
-        assertEquals ("[RECTANGLE, POLYGON]", CANVAS.getCommands().toString());
+        MyCanvas testCanvas = new MyCanvas();
+
+        System.out.println("test case get command");
+        testCanvas.addCommand("RECTANGLE");
+        testCanvas.addCommand("POLYGON");
+        assertEquals ("[RECTANGLE, POLYGON]", testCanvas.getCommands().toString());
     }
 
-    @org.junit.jupiter.api.Test
-    void paintComponent() {
-
-
-    }
-
-    @org.junit.jupiter.api.Test
-    void mousePressed() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void mouseReleased() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void mouseEntered() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void mouseDragged() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void mouseMoved() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void mouseExited() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void mouseClicked() {
-    }
 }
--- src/org/qut/MyCoordinateTest.java
+++ src/org/qut/MyCoordinateTest.java
@@ -8,26 +8,47 @@ class MyCoordinateTest {
 
     @org.junit.jupiter.api.Test
     void setXY() {
+        MyCoordinate testCoordinates = new MyCoordinate(0,0);
 
+        System.out.println("test case set X and Y command");
+        testCoordinates.setXY(5,5);
+        assertEquals(5, testCoordinates.getX());
+        assertEquals(5, testCoordinates.getY());
     }
 
     @org.junit.jupiter.api.Test
     void getX() {
+        MyCoordinate testCoordinates = new MyCoordinate(10,10);
 
+        System.out.println("test case get X command");
+        Double testX = testCoordinates.getX();
+        assertEquals(10, testX);
     }
 
     @org.junit.jupiter.api.Test
     void setX() {
+        MyCoordinate testCoordinates = new MyCoordinate(10,10);
 
+        System.out.println("test case set X command");
+        testCoordinates.setX(5);
+        assertEquals(5, testCoordinates.getX());
     }
 
     @org.junit.jupiter.api.Test
     void getY() {
+        MyCoordinate testCoordinates = new MyCoordinate(10,10);
 
+        System.out.println("test case get Y command");
+        Double testY = testCoordinates.getY();
+        assertEquals(10, testY);
     }
 
     @org.junit.jupiter.api.Test
     void setY() {
+        MyCoordinate testCoordinates = new MyCoordinate(10,10);
 
+        System.out.println("test case set Y command");
+        testCoordinates.setY(5);
+        assertEquals(5, testCoordinates.getY());
     }
 }
